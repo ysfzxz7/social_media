dbb9d3e17576d7eef6ae4b212afcdc9979e66271
ysfzxz7 <bbbmaroc83@gmail.com>
2024-02-09 18:38:16 +0100

    page posts

--- social_media/src/app/posts/page.jsx
+++ social_media/src/app/posts/page.jsx
@@ -19,24 +19,22 @@ export default async function Posts() {
 
         return (
         <div>
-            <h1 className="bg-green-600 p-3 text-center">Post page, Here you can find all Posts</h1>
-        <div className="flex justify-center space-x-2  space-y-2 flex-wrap p-2">
+            <div className="border relative  flex justify-center space-x-1 h-[95vh]    space-y-3 flex-wrap p-3">
+            <h1 className=""></h1>
             {
             todos.map(todo =>(
-                    <Link href={`posts/${todo.id}`}  className="border w-[32%] p-5 shadow-md rounded bg-gray-100/5">
+                    <Link href={`posts/${todo.id}`}  className=" px-4 border w-[32%] bg-slate-400/5 p-5 shadow-md rounded ">
                         <div className=" space-y-3">
                             <h1 className="w-fit items-center rounded-md bg-yellow-50 px-2 py-1 text-xs font-medium text-yellow-800 ring-1 ring-inset ring-yellow-600/20 ">{getUserName(todo.userId)}</h1>
-                            <h2 className="inline-flex items-center rounded-md bg-gray-50 px-2 py-1 text-xs font-medium text-gray-600 ring-1 ring-inset ring-gray-500/10">{todo.title}</h2>
+                            <h2 className="inline-flex items-center rounded-md bg-white px-2 py-1 text-xs font-medium text-gray-600 ring-1 ring-inset ring-gray-500/10">{todo.title}</h2>
                             <p className="text-xs my-3">{todo.body}</p>
                             <div class=" text-white bg-bleu-500 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300  rounded text-xs px-3 py-1 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800  w-fit">Read more</div>
-
-
                         </div>
                     </Link>
                 ))}
         </div>
 
+    
         </div>
-
     )
 }
--- social_media/src/app/users/page.js
+++ social_media/src/app/users/page.js
@@ -7,15 +7,15 @@ const users = await res.json()
     return (
         <div className="w-[100vw] h-[95vh] bg-slate-300 flex items-center justify-center">
 
-        <div className="w-[35%] rounded bg-zinc-600/60 ring-1 mx-auto  border p-5 ">
-            <h1 className="text-xl font-bold  ">all users</h1>
+        <div className="w-[35%] rounded bg-white/60 ring-1 shadow-lg mx-auto  border p-5 ">
+            <h1 className="font-medium">Active users</h1>
             {
                 users.map((user)=>(
-                    <div key={user.id} className=" flex flex-row justify-between items-center space-y-2 border-t-gray-500 ">
-                        <h1 className="bg-gray-400 w-10 text-center h-10 font-medium flex items-center justify-center text-white rounded-[100%]">{user.name.substring(0,2).toUpperCase()}</h1>
-                        <div className="w-[80%]">
+                    <div key={user.id} className="  flex flex-row justify-center  items-center space-y-3 border-t-2 ">
+                        <h1 className="  user_pic mt-3 mr-5 bg-gray-400 w-10 text-center h-10 font-medium flex items-center justify-center text-white rounded-[100%]">{user.name.substring(0,2).toUpperCase()}</h1>
+                        <div className="w-[80%] ">
                             <h1 className="text-sm font-medium truncate">{user.name}</h1>
-                            <p class="text-sm text-white truncate ">{user.email}</p>
+                            <p class="text-sm text-gray-500 truncate ">{user.email}</p>
                         </div>
                         <h1 className="bg-[rgba(63,131,248,.5)] text-center h-6 w-6 rounded-full">{user.id}</h1>
                         
